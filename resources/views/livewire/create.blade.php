701309c4985a4960ce47dd0749fa7ea705d35dca
Kabir Deula <kabirdeula167@gmail.com>
2023-07-14 19:39:41 +0545

    added CRUD

---
+++ resources/views/livewire/create.blade.php
@@ -0,0 +1,34 @@
+<form>
+    <div class="form-group mb-3">
+        <label for="category_name">Name:</label>
+
+        <input type="text" id="category_name" class="form-control @error('name')
+            is-invalid @enderror"
+            placeholder="Enter Name" wire:model.lazy="name">
+
+        @error('name')
+            <span class="text-danger">
+                {{ $message }}
+            </span>
+        @enderror
+    </div>
+
+    <div class="form-group mb-3">
+        <label for="category_descripton">Description:</label>
+
+        <input type="text" id="category_descripton"
+            class="form-control @error('description')
+            is-invalid @enderror" placeholder="Enter Description"
+            wire:model.lazy="description">
+
+        @error('description')
+            <span class="text-danger">
+                {{ $message }}
+            </span>
+        @enderror
+    </div>
+
+    <div class="d-grid gap-2">
+        <button wire:click.prevent="store()" class="btn btn-success btn-block">Save</button>
+    </div>
+</form>
